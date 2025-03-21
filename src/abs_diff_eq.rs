ed842aacf405248182beca1ba66a73a6eac3962e
amascolo <12738821+amascolo@users.noreply.github.com>
2025-03-21 21:14:04 +0000

    AbsDiffEq for tuples

--- src/abs_diff_eq.rs
+++ src/abs_diff_eq.rs
@@ -243,6 +243,59 @@ where
     }
 }
 
+#[cfg(feature = "tuple_impl")]
+macro_rules! impl_abs_diff_eq {
+    () => {
+        impl AbsDiffEq for () {
+            type Epsilon = ();
+
+            fn default_epsilon() -> Self::Epsilon {
+                ()
+            }
+
+            fn abs_diff_eq(&self, _other: &Self, _epsilon: Self::Epsilon) -> bool {
+                true
+            }
+        }
+    };
+
+    ($($T:ident),+ ; $($idx:tt),+) => {
+        impl<$($T),+> AbsDiffEq for ($($T,)+)
+        where
+            $($T: AbsDiffEq,)+
+        {
+            type Epsilon = ($($T::Epsilon,)+);
+
+            fn default_epsilon() -> Self::Epsilon {
+                ($($T::default_epsilon(),)+)
+            }
+
+            fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+                true $(&& self.$idx.abs_diff_eq(&other.$idx, epsilon.$idx))+
+            }
+        }
+    };
+}
+
+#[cfg(feature = "tuple_impl")]
+mod abs_diff_eq_tuple_impls {
+    use super::*;
+
+    impl_abs_diff_eq!();
+    impl_abs_diff_eq!(T0; 0);
+    impl_abs_diff_eq!(T0, T1; 0, 1);
+    impl_abs_diff_eq!(T0, T1, T2; 0, 1, 2);
+    impl_abs_diff_eq!(T0, T1, T2, T3; 0, 1, 2, 3);
+    impl_abs_diff_eq!(T0, T1, T2, T3, T4; 0, 1, 2, 3, 4);
+    impl_abs_diff_eq!(T0, T1, T2, T3, T4, T5; 0, 1, 2, 3, 4, 5);
+    impl_abs_diff_eq!(T0, T1, T2, T3, T4, T5, T6; 0, 1, 2, 3, 4, 5, 6);
+    impl_abs_diff_eq!(T0, T1, T2, T3, T4, T5, T6, T7; 0, 1, 2, 3, 4, 5, 6, 7);
+    impl_abs_diff_eq!(T0, T1, T2, T3, T4, T5, T6, T7, T8; 0, 1, 2, 3, 4, 5, 6, 7, 8);
+    impl_abs_diff_eq!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    impl_abs_diff_eq!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    impl_abs_diff_eq!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+}
+
 #[cfg(feature = "num-complex")]
 impl<T: AbsDiffEq> AbsDiffEq for Complex<T>
 where
